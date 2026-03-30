71c2d23b4293a2a409a18592df488d5b9ad2bdcf
Dung Nguyen <ct070212@actvn.edu.vn>
2026-03-30 20:41:19 +0700

    fix

--- ChatDriver/kernel_module/chat_crypto.c
+++ ChatDriver/kernel_module/chat_crypto.c
@@ -8,13 +8,11 @@
 #include <linux/scatterlist.h>
 #include <linux/string.h>
 
-#define CRYPTO_IOCTL_SHA1    _IOWR('c', 1, struct crypto_req)
-#define CRYPTO_IOCTL_DES_ENC _IOWR('c', 2, struct crypto_req)
-#define CRYPTO_IOCTL_DES_DEC _IOWR('c', 3, struct crypto_req)
+#include "../include/crypto_module.h"
 
 // Device configuration
-#define DEVICE_NAME "kma_crypto"
-#define CLASS_NAME  "kma_crypto_class"
+#define DEVICE_NAME "chat_crypto"
+#define CLASS_NAME  "chat_crypto_class"
 
 // Device state (global)
 static int dev_major = 0;
@@ -22,17 +20,8 @@ static struct class *crypto_class = NULL;
 static struct device *crypto_device = NULL;
 static struct cdev crypto_cdev;
 
-// Cau truc goi tin giao tiep giua App va Kernel
-struct crypto_req {
-    char data[256];
-    int data_len;
-    char key[8];    // Key 8 byte cho DES
-    char result[256];
-    int result_len;
-};
-
 // Ham bam SHA1
-static int do_sha1(struct crypto_req *req) {
+static int do_sha1(struct sha1_request *req) {
     struct crypto_hash *tfm;
     struct hash_desc desc;
     struct scatterlist sg;
@@ -43,33 +32,32 @@ static int do_sha1(struct crypto_req *req) {
     desc.tfm = tfm;
     desc.flags = 0;
 
-    sg_init_one(&sg, req->data, req->data_len);
+    sg_init_one(&sg, req->input, req->input_len);
     crypto_hash_init(&desc);
-    crypto_hash_update(&desc, &sg, req->data_len);
-    crypto_hash_final(&desc, req->result);
+    crypto_hash_update(&desc, &sg, req->input_len);
+    crypto_hash_final(&desc, req->digest);
 
-    req->result_len = crypto_hash_digestsize(tfm);
     crypto_free_hash(tfm);
     return 0;
 }
 
 // Ham ma hoa / giai ma DES
-static int do_des(struct crypto_req *req, int encrypt) {
+static int do_des(struct des_request *req) {
     struct crypto_blkcipher *tfm;
     struct blkcipher_desc desc;
     struct scatterlist sg_in, sg_out;
-    int ret, crypt_len = req->data_len;
+    int ret, crypt_len = req->input_len;
 
     // Thuat toan DES khoi (ECB) yeu cau du lieu phai là boi so cua 8 byte.
     if (crypt_len % 8 != 0) {
         crypt_len = ((crypt_len / 8) + 1) * 8;
-        memset(req->data + req->data_len, 0, crypt_len - req->data_len); // Dem so 0 vao cuoi
+        memset(req->input + req->input_len, 0, crypt_len - req->input_len);
     }
 
     tfm = crypto_alloc_blkcipher("ecb(des)", 0, CRYPTO_ALG_ASYNC);
     if (IS_ERR(tfm)) return PTR_ERR(tfm);
 
-    ret = crypto_blkcipher_setkey(tfm, req->key, 8);
+    ret = crypto_blkcipher_setkey(tfm, req->key, DES_KEY_SIZE);
     if (ret) {
         crypto_free_blkcipher(tfm);
         return ret;
@@ -78,35 +66,46 @@ static int do_des(struct crypto_req *req, int encrypt) {
     desc.tfm = tfm;
     desc.flags = 0;
 
-    sg_init_one(&sg_in, req->data, crypt_len);
-    sg_init_one(&sg_out, req->result, crypt_len);
+    sg_init_one(&sg_in, req->input, crypt_len);
+    sg_init_one(&sg_out, req->output, crypt_len);
 
-    if (encrypt) ret = crypto_blkcipher_encrypt(&desc, &sg_out, &sg_in, crypt_len);
-    else         ret = crypto_blkcipher_decrypt(&desc, &sg_out, &sg_in, crypt_len);
+    if (req->mode == 0) ret = crypto_blkcipher_encrypt(&desc, &sg_out, &sg_in, crypt_len);
+    else                ret = crypto_blkcipher_decrypt(&desc, &sg_out, &sg_in, crypt_len);
 
-    req->result_len = crypt_len;
+    req->output_len = crypt_len;
     crypto_free_blkcipher(tfm);
     return ret;
 }
 
 // Ham lang nghe su kien tu User-space
 static long crypto_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
-    struct crypto_req req;
-
-    if (copy_from_user(&req, (struct crypto_req *)arg, sizeof(req)))
-        return -EFAULT;
+    int ret = 0;
 
     switch (cmd) {
-        case CRYPTO_IOCTL_SHA1:     do_sha1(&req); break;
-        case CRYPTO_IOCTL_DES_ENC:  do_des(&req, 1); break;
-        case CRYPTO_IOCTL_DES_DEC:  do_des(&req, 0); break;
-        default: return -EINVAL;
+        case CRYPTO_IOCTL_SHA1_HASH: {
+            struct sha1_request req;
+            if (copy_from_user(&req, (void *)arg, sizeof(req)))
+                return -EFAULT;
+            ret = do_sha1(&req);
+            if (copy_to_user((void *)arg, &req, sizeof(req)))
+                return -EFAULT;
+            break;
+        }
+        case CRYPTO_IOCTL_DES_ENCRYPT:
+        case CRYPTO_IOCTL_DES_DECRYPT: {
+            struct des_request req;
+            if (copy_from_user(&req, (void *)arg, sizeof(req)))
+                return -EFAULT;
+            ret = do_des(&req);
+            if (copy_to_user((void *)arg, &req, sizeof(req)))
+                return -EFAULT;
+            break;
+        }
+        default:
+            return -EINVAL;
     }
 
-    if (copy_to_user((struct crypto_req *)arg, &req, sizeof(req)))
-        return -EFAULT;
-
-    return 0;
+    return ret;
 }
 
 // Device open handler
--- ChatDriver/kernel_module/chat_crypto_test.c
+++ ChatDriver/kernel_module/chat_crypto_test.c
@@ -5,55 +5,121 @@
 #include <sys/ioctl.h>
 #include <unistd.h>
 
-#define CRYPTO_IOCTL_SHA1    _IOWR('c', 1, struct crypto_req)
-#define CRYPTO_IOCTL_DES_ENC _IOWR('c', 2, struct crypto_req)
-#define CRYPTO_IOCTL_DES_DEC _IOWR('c', 3, struct crypto_req)
-
-struct crypto_req {
-    char data[256];
-    int data_len;
-    char key[8];
-    char result[256];
-    int result_len;
-};
+#include "../include/crypto_module.h"
 
 int main(int argc, char *argv[]) {
-    int i;
-    if (argc < 3) return -1;
+    int i, fd, ret;
+    if (argc < 3) {
+        printf("Usage:\n");
+        printf("  %s sha1 <text>              - Hash text\n", argv[0]);
+        printf("  %s enc <text> <key>         - Encrypt (DES, ECB mode)\n", argv[0]);
+        printf("  %s dec <hex> <key>          - Decrypt (DES, ECB mode)\n", argv[0]);
+        return 1;
+    }
 
-    int fd = open("/dev/kma_crypto", O_RDWR);
-    if (fd < 0) { perror("Khong the mo /dev/kma_crypto"); return -1; }
+    fd = open(CRYPTO_DEVICE_FILE, O_RDWR);
+    if (fd < 0) {
+        perror("Cannot open " CRYPTO_DEVICE_FILE);
+        return 1;
+    }
 
-    struct crypto_req req;
-    memset(&req, 0, sizeof(req));
-
-    strncpy(req.data, argv[2], 255);
-    req.data_len = strlen(req.data);
-
+    // SHA1 HASH
     if (strcmp(argv[1], "sha1") == 0) {
-        ioctl(fd, CRYPTO_IOCTL_SHA1, &req);
-        for (i = 0; i < req.result_len; i++) printf("%02x", (unsigned char)req.result[i]);
+        struct sha1_request req;
+        memset(&req, 0, sizeof(req));
+
+        strncpy((char *)req.input, argv[2], SHA1_MAX_INPUT - 1);
+        req.input_len = strlen((char *)req.input);
+
+        ret = ioctl(fd, CRYPTO_IOCTL_SHA1_HASH, &req);
+        if (ret < 0) {
+            perror("SHA1 ioctl failed");
+            close(fd);
+            return 1;
+        }
+
+        printf("%s: ", argv[2]);
+        for (i = 0; i < SHA1_DIGEST_SIZE; i++)
+            printf("%02x", req.digest[i]);
         printf("\n");
     }
+    // DES ENCRYPT
     else if (strcmp(argv[1], "enc") == 0) {
-        strncpy(req.key, argv[3], 8);
-        ioctl(fd, CRYPTO_IOCTL_DES_ENC, &req);
-        for (i = 0; i < req.result_len; i++) printf("%02x", (unsigned char)req.result[i]);
+        struct des_request req;
+        memset(&req, 0, sizeof(req));
+
+        if (strlen(argv[3]) != DES_KEY_SIZE) {
+            printf("Error: Key must be exactly %d bytes\n", DES_KEY_SIZE);
+            close(fd);
+            return 1;
+        }
+
+        strncpy((char *)req.key, argv[3], DES_KEY_SIZE);
+        strncpy((char *)req.input, argv[2], MAX_CRYPTO_DATA - 1);
+        req.input_len = strlen((char *)req.input);
+
+        // Pad to multiple of 8 bytes
+        if (req.input_len % 8 != 0) {
+            req.input_len = ((req.input_len / 8) + 1) * 8;
+        }
+
+        req.mode = 0;  // 0 = encrypt
+
+        ret = ioctl(fd, CRYPTO_IOCTL_DES_ENCRYPT, &req);
+        if (ret < 0) {
+            perror("DES encrypt ioctl failed");
+            close(fd);
+            return 1;
+        }
+
+        printf("Encrypted: ");
+        for (i = 0; i < req.output_len; i++)
+            printf("%02x", req.output[i]);
         printf("\n");
     }
+    // DES DECRYPT
     else if (strcmp(argv[1], "dec") == 0) {
-        strncpy(req.key, argv[3], 8);
+        struct des_request req;
+        memset(&req, 0, sizeof(req));
+
+        if (strlen(argv[3]) != DES_KEY_SIZE) {
+            printf("Error: Key must be exactly %d bytes\n", DES_KEY_SIZE);
+            close(fd);
+            return 1;
+        }
+
+        strncpy((char *)req.key, argv[3], DES_KEY_SIZE);
+
+        // Parse hex string
         int hex_len = strlen(argv[2]) / 2;
-        req.data_len = hex_len;
+        if (hex_len % 8 != 0) {
+            printf("Error: Hex data length must be multiple of 8 bytes\n");
+            close(fd);
+            return 1;
+        }
+
         for (i = 0; i < hex_len; i++) {
             unsigned int byte;
             sscanf(&argv[2][i*2], "%2x", &byte);
-            req.data[i] = (char)byte;
+            req.input[i] = (unsigned char)byte;
         }
-        ioctl(fd, CRYPTO_IOCTL_DES_DEC, &req);
-        printf("%s\n", req.result);
+        req.input_len = hex_len;
+        req.mode = 1;  // 1 = decrypt
+
+        ret = ioctl(fd, CRYPTO_IOCTL_DES_DECRYPT, &req);
+        if (ret < 0) {
+            perror("DES decrypt ioctl failed");
+            close(fd);
+            return 1;
+        }
+
+        printf("Decrypted: %.*s\n", (int)req.output_len, req.output);
+    }
+    else {
+        printf("Unknown command: %s\n", argv[1]);
+        close(fd);
+        return 1;
     }
 
     close(fd);
     return 0;
-}
